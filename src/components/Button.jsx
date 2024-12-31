8f1b072c66d5b4c60c414895b96c062036d3f4be
abhishek-akp <abhishekkumarpandey2803@gmail.com>
2024-12-31 15:16:44 -0500

    Further dev

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({name, isBeam = false, containerClass}) => {
   return (
-    <button className={`btn $${containerClass} bg-opacity-50`}>
+    <button className={`btn ${containerClass} bg-opacity-50`}>
         {isBeam && (<span className='relative flex h-3 w-3'>
             <span className='btn-ping'/>
             <span className='btn-ping_dot'/>
--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
     const toggleMenu = () => setIsOpen((prevIsOpen) => !prevIsOpen);
   return (
     <header className='fixed top-0 right-0 left-0 z-50 bg-black/90'>
-        <div className='max-w-8xl mx-auto'>
+        <div className='max-w-7xl mx-auto'>
             <div className='flex items-center justify-between py-5 mx-auto c-space'>
                 <a href='/' className='text-neutral-400 font-bold text-xl hover:text-white transition-colors'>Abhishek Kumar Pandey</a>
                 <button onClick={toggleMenu} className='text-neutral-400 hover:text-white focus:outline-none sm:hidden flex' aria-label='Toggle Menu'>
