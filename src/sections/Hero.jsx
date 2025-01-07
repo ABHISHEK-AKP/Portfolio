37b4e1d5e9d8d8946c4d1013fd98bf8bb4dd7b26
abhishek-akp <abhishekkumarpandey2803@gmail.com>
2025-01-06 19:34:49 -0500

    adding navigations

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -30,7 +30,7 @@ const  Hero = () => {
     const isTablet = useMediaQuery({minWidth:768, maxWidth:1024});
     const sizes = calculateSizes(isSmall, isMobile, isTablet);
   return (
-    <section className='min-h-screen w-full flex flex-col relative'> 
+    <section className='min-h-screen w-full flex flex-col relative' id='home'> 
     <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3'>
         <p className='sm:text-3xl text-xl font-medium text-white text-center font-generalsans'>Hi, I am Abhishek<span className='waving-hand'>👋🏻</span></p>
         <p className='hero_tag text-gray_gradient'>Building Web Applications</p>
